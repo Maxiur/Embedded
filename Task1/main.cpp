e8b349199c742033bdc3fa32a629f4f875077729
Maxiur <mateusz222244@op.pl>
2026-03-14 16:38:07 +0100

    weighs added

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <utility>
 
-std::vector<std::vector<int>> generateGraph(int tasks, int uni, int dedi, int lines) {
+std::vector<std::vector<std::pair<int, int>>> generateGraph(int tasks) {
     std::random_device rd;
     std::mt19937_64 gen(rd());
-    std::uniform_int_distribution<> dist(0, 1);
-    std::vector<std::vector<int>> graph(tasks);
+    std::uniform_int_distribution<> dist(1, 100);
+    std::uniform_int_distribution<> dist_weigh(1, 100);
+    std::vector<std::vector<std::pair<int, int>>> graph(tasks);
 
-    for (int i = 0; i < tasks; i++) {
+    for (int i = 1; i < tasks; i++) {
         for (int j = 0; j < i; j++) {
-            if (dist(gen) == 1) {
-                graph[i].push_back(j);
+            // 60%
+            if (dist(gen) <= 60) {
+                int weight = dist_weigh(gen);
+                graph[i].push_back({j, weight});
             }
+        }
+        if (graph[i].empty()) {
+            std::uniform_int_distribution dist_forced(0, i - 1);
+            int forced_parent = dist_forced(gen);
+            int weight = dist_weigh(gen);
+            graph[i].push_back({forced_parent, weight});
         }
     }
     return graph;
@@ -33,6 +43,17 @@ bool is_cyclic(int u, std::vector<std::vector<int>>& graph, std::vector<int>& co
     return false;
 }
 
+void printGraph(std::vector<std::vector<int>>& graph) {
+    int i = 0;
+    for (auto row : graph) {
+        std::cout << i++ << " ";
+        for (auto col : row) {
+            std::cout << col << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 6) {
         std::cerr << "Brakuje danych! Nalezy wpisać - Liczbe zadań, jednostek dedykowanych, uniwersalnych, liczbę lin komunikacyjnych oraz nazwe pliku wyjsciowego\n";
@@ -47,7 +68,27 @@ int main(int argc, char* argv[]) {
 
     std::cout << tasks << " " << uni << " " << dedi << " " << lines << " " << output << std::endl;
 
-    std::vector<std::vector<int>> adjList = generateGraph(tasks);
+    // std::vector<std::vector<int>> adjList = generateGraph(tasks);
+    // printGraph(adjList);
+    
+for (int i = 0; i < 10000; i++) {
+    auto graph = generateGraph(tasks);
+
+    std::vector<int> colors(tasks, 0);
+
+    bool cycle_found = false;
+    for (int v = 0; v < tasks; ++v) {
+        if (colors[v] == 0 && is_cyclic(v, graph, colors)) {
+            cycle_found = true;
+            break;
+        }
+    }
+
+    if (cycle_found) {
+        std::cout << "O kurcze, wylosowalo cykl!\n";
+        printGraph(graph); // Zakładam, że taką funkcję już masz
+    }
+}
 
 
     return 0;
