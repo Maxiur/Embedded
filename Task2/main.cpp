57450e70b54447a75eb52f27380fcb60da0f2f64
Maxiur <mateusz222244@op.pl>
2026-03-22 21:41:37 +0100

    read graph

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -2,37 +2,112 @@
 #include <fstream>
 #include <vector>
 
-std::vector<std::vector<std::pair<int, int>>> readGraph(std::string& file_name, std::istream& in) {
+struct Edge {
+    int target;
+    int weight;
+};
+
+struct Processor {
+    int cost;
+    int type; // 0 - HC, 1 - PP
+};
+
+struct Channel {
+    int cost;
+    int bandwidth;
+    std::vector<int> connected_processor;
+};
+
+struct TaskGraph {
+    int tasks;
+    int proc;
+    int bus;
+
+    std::vector<std::vector<Edge>> graph;
+    std::vector<Processor> processors;
+    std::vector<Channel> channels;
+    std::vector<std::vector<int>> times;
+    std::vector<std::vector<int>> costs;
+};
+
+TaskGraph readGraph(const std::string& file_name) {
     std::ifstream input(file_name);
+    TaskGraph graph;
+
     if (!input) {
         std::cerr << "Nie mozna otworzyc pliku wejsciowego\n";
-        return {};
+        return graph;
     }
 
-    std::vector<std::vector<std::pair<int, int>>> graph;
-    std::string line;
+    std::string trash_string;
+    char trash_char;
 
-    int tasks = 0;
-    if (std::getline(input, line)) {
-        std::string target = "@tasks ";
-        size_t position = line.find(target);
-        if (position != std::string::npos) {
-            std::string valueString = line.substr(position + target.length());
-            tasks = std::stoi(valueString);
+    // Zadania graf (@tasks)
+    input >> trash_string >> graph.tasks;
+    graph.graph.resize(graph.tasks);
+
+    for (int i = 0; i < graph.tasks; ++i) {
+        int edges;
+        input >> trash_string >> edges;
+
+        for (int e = 0; e < edges; ++e) {
+            int target, weight;
+            input >> target >> trash_char >> weight >> trash_char;
+            graph.graph[i].emplace_back(target, weight);
         }
-        std::cout << tasks << std::endl;
     }
 
-    for (int i = 0; i < tasks; i++) {
+    // Procesory (@proc)
+    input >> trash_string >> graph.proc;
+    graph.processors.resize(graph.proc);
 
+    for (int i = 0; i < graph.proc; ++i) {
+        int dummy_zero;
+        input >> graph.processors[i].cost >> dummy_zero >> graph.processors[i].type;
+    }
+
+    // Czasy (@times)
+    input >> trash_string;
+    graph.times.resize(graph.tasks, std::vector<int>(graph.proc));
+
+    for (int i = 0; i < graph.tasks; ++i) {
+        for (int j = 0; j < graph.proc; ++j) {
+            input >> graph.times[i][j];
+        }
+    }
+
+    // Koszty (@cost)
+    input >> trash_string;
+    graph.costs.resize(graph.tasks, std::vector<int>(graph.proc));
+
+    for (int i = 0; i < graph.tasks; ++i) {
+        for (int j = 0; j < graph.proc; ++j) {
+            input >> graph.costs[i][j];
+        }
+    }
+
+    // Kanały (@comm)
+    input >> trash_string >> graph.bus;
+    graph.channels.resize(graph.bus);
+
+    for (int i = 0; i < graph.bus; ++i) {
+        input >> trash_string >> graph.channels[i].cost >> graph.channels[i].bandwidth;
+        graph.channels[i].connected_processor.resize(graph.proc);
+
+        for (int j = 0; j < graph.proc; ++j) {
+            input >> graph.channels[i].connected_processor[j];
+        }
     }
 
     return graph;
+
 }
 
 int main() {
-    std::string input = "../input.txt";
+    std::string input= "../input.txt";
+    TaskGraph system = readGraph(input);
 
-    readGraph(input, std::cin);
+
+
     return 0;
 }
