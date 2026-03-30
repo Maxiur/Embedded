fbe23d3edfaf7756d06fdb57f6db67c67c4e98a2
Maxiur <mateusz222244@op.pl>
2026-03-30 22:17:07 +0200

    almost

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -127,7 +127,22 @@ std::pair<std::vector<int>, std::vector<int>> LowestTime(const TaskGraph& graph)
     return { TaskProcessors, TaskTimes };
 }
 
-std::tuple<std::vector<int>, std::vector<int>, int> BFS(const TaskGraph& graph, const std::vector<int>& TaskTimes, const std::vector<int>& TaskProcessors) {
+int FindChannelIndex(const TaskGraph& graph, int Processor) {
+    int max_bandwidth = INT_MIN;
+    int max_channel = -1;
+
+    for (int i = 0; i < graph.bus; ++i) {
+        if (graph.channels[i].connected_processor[Processor] == 1) {
+            if (graph.channels[i].bandwidth > max_bandwidth) {
+                max_bandwidth = graph.channels[i].bandwidth;
+                max_channel = i;
+            }
+        }
+    }
+    return max_channel;
+}
+
+std::tuple<std::vector<double>, std::vector<double>, double> BFS(const TaskGraph& graph, const std::vector<int>& TaskTimes, const std::vector<int>& TaskProcessors) {
     std::vector in_degree(graph.tasks, 0);
 
     for (int i = 0; i < graph.tasks; ++i) {
@@ -143,10 +158,10 @@ std::tuple<std::vector<int>, std::vector<int>, int> BFS(const TaskGraph& graph, 
         }
     }
 
-    std::vector task_start_time(graph.tasks, 0);
-    std::vector earliest_start(graph.tasks, 0);
-    std::vector task_finish_time(graph.tasks, 0);
-    int makespan = 0;
+    std::vector task_start_time(graph.tasks, 0.0);
+    std::vector earliest_start(graph.tasks, 0.0);
+    std::vector task_finish_time(graph.tasks, 0.0);
+    double makespan = 0.0;
 
     while (!queue.empty()) {
         int current = queue.front();
@@ -158,8 +173,16 @@ std::tuple<std::vector<int>, std::vector<int>, int> BFS(const TaskGraph& graph, 
 
         for (const auto& edge : graph.graph[current]) {
 
+            int channel = FindChannelIndex(graph, TaskProcessors[current]);
+            double communication_time = static_cast<double>(edge.weight) / graph.channels[channel].bandwidth;
+
             // Czekamy na rodziców
-            earliest_start[edge.target] = std::max(earliest_start[edge.target], task_finish_time[current]);
+            if (TaskProcessors[current] != TaskProcessors[edge.target]) {
+                earliest_start[edge.target] = std::max(earliest_start[edge.target], task_finish_time[current] + communication_time);
+            }
+            else {
+                earliest_start[edge.target] = std::max(earliest_start[edge.target], task_finish_time[current]);
+            }
 
             in_degree[edge.target]--;
             if (in_degree[edge.target] == 0) {
@@ -170,60 +193,23 @@ std::tuple<std::vector<int>, std::vector<int>, int> BFS(const TaskGraph& graph, 
     return { task_start_time, task_finish_time, makespan };
 }
 
-Channel FindChannel(const TaskGraph& graph, std::vector<int> TaskProcessors, int TaskIndex) {
-
-}
-
-int CalculateCost(const TaskGraph& graph, const std::vector<int>& TaskProcessors) {
-    int total_cost = 0;
-    std::vector<int> pe_usage(graph.proc, 0);
-
-    for (int i = 0; i < graph.tasks; ++i) {
-        int p = TaskProcessors[i];
-        pe_usage[p]++;
-        total_cost += graph.costs[i][p];
-    }
-
-    for (int p = 0; p < graph.proc; ++p) {
-        if (pe_usage[p] > 0 && graph.processors[p].type == 1) {
-            total_cost += graph.processors[p].cost;
-        }
-    }
-
-    for (int c = 0; c < graph.bus; ++c) {
-        for (int p = 0; p < graph.proc; ++p) {
-            if (pe_usage[p] > 0 && graph.channels[c].connected_processor[p] == 1) {
-                if (graph.processors[p].type == 1) {
-                    total_cost += graph.channels[c].cost;
-                }
-                else {
-                    total_cost += graph.channels[c].cost * pe_usage[p];
-                }
-            }
-        }
-    }
-
-    return total_cost;
-}
-
 int main() {
     std::string input;
     std::cout << "Podaj nazwe pliku wejsciowego (razem z .txt): " << std::endl;
     std::cin >> input;
-    TaskGraph graph = readGraph(input);
+    TaskGraph graph = readGraph("../" + input);
 
     auto [TaskProcessors, TaskTimes] = LowestTime(graph);
     auto [task_start_time, task_finish_time, makespan] = BFS(graph, TaskTimes, TaskProcessors);
 
     std::cout << "Najszybszy czas calkowity (Makespan): " << makespan << "\n\n";
-    std::cout << "Koszt wykonania zadania: " << CalculateCost(graph, TaskProcessors) << "\n\n";
     std::cout << "Wykorzystana Architektura:\n";
 
+    std::vector<std::string> proc_names(graph.proc);
     int hc_counter = 0;
     int pp_counter = 0;
 
     for (int p = 0; p < graph.proc; ++p) {
-
         std::string prefix;
         if (graph.processors[p].type == 0) {
             prefix = "HC" + std::to_string(hc_counter++);
@@ -231,6 +217,7 @@ int main() {
         else {
             prefix = "PP" + std::to_string(pp_counter++);
         }
+        proc_names[p] = prefix;
 
         std::cout << prefix << ": ";
 
@@ -256,6 +243,42 @@ int main() {
         std::cout << "\n";
     }
 
+    // Dodane: Sprawdzamy, które kanały zostały użyte i wypisujemy
+    std::cout << "\nWykorzystane Kanaly:\n";
+    std::vector<bool> used_channels(graph.bus, false);
+
+    for (int i = 0; i < graph.tasks; ++i) {
+        for (const auto& edge : graph.graph[i]) {
+            if (TaskProcessors[i] != TaskProcessors[edge.target]) {
+                int channel = FindChannelIndex(graph, TaskProcessors[i]);
+                if (channel != -1) {
+                    used_channels[channel] = true;
+                }
+            }
+        }
+    }
+
+    bool any_channel_used = false;
+    for (int c = 0; c < graph.bus; ++c) {
+        if (used_channels[c]) {
+            any_channel_used = true;
+            std::cout << "CHAN" << c << ": ";
+
+            bool first_conn = true;
+            for (int p = 0; p < graph.proc; ++p) {
+                if (graph.channels[c].connected_processor[p] == 1) {
+                    if (!first_conn) std::cout << ", ";
+                    std::cout << proc_names[p];
+                    first_conn = false;
+                }
+            }
+            std::cout << "\n";
+        }
+    }
+
+    if (!any_channel_used) {
+        std::cout << "brak\n";
+    }
 
     return 0;
 }
