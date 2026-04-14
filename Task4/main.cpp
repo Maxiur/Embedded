c523b422838b08f320ed448253740ace02096a5a
Maxiur <mateusz222244@op.pl>
2026-04-14 19:07:48 +0200

    readme

--- Task4/main.cpp
+++ Task4/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <tuple>
 #include <queue>
+#include <climits>
+#include <algorithm>
+#include <utility>
+#include <string>
 
 struct Edge {
     int target;
     int weight;
-    int totalSum{};
 };
 
 struct Processor {
@@ -20,7 +20,7 @@ struct Processor {
 struct Channel {
     int cost{};
     int bandwidth{};
-    std::vector<bool> connected_processor;
+    std::vector<int> connected_processor;
 };
 
 struct TaskGraph {
@@ -100,126 +100,158 @@ TaskGraph readGraph(const std::string& file_name) {
         graph.channels[i].connected_processor.resize(graph.proc);
 
         for (int j = 0; j < graph.proc; ++j) {
-            int temp;
-            input >> temp;
-            graph.channels[i].connected_processor[j] = temp;
+            input >> graph.channels[i].connected_processor[j];
         }
     }
 
     return graph;
 }
 
-int CalculateDP(TaskGraph& graph, int TaskIndex, int PP_ProcessorID, std::vector<int>& dp) {
-    if (dp[TaskIndex] != -1) {
-        return dp[TaskIndex];
+std::pair<std::vector<int>, std::vector<int>> LowestTime(const TaskGraph& graph) {
+    std::vector<int> TaskProcessors(graph.tasks);
+    std::vector<int> TaskTimes(graph.tasks);
+
+    for (int i = 0; i < graph.tasks; i++) {
+        int min_time = INT_MAX;
+        int min_proc = -1;
+        for (int j = 0; j < graph.proc; j++) {
+            if (graph.times[i][j] != -1 && min_time > graph.times[i][j]) {
+                min_time = graph.times[i][j];
+                min_proc = j;
+            }
+        }
+        TaskProcessors[i] = min_proc;
+        TaskTimes[i] = min_time;
     }
 
-    int max_child_time = 0;
-
-    for (const auto& edge : graph.graph[TaskIndex] ) {
-        max_child_time = std::max(max_child_time, CalculateDP(graph, edge.target, PP_ProcessorID, dp));
-    }
-
-    dp[TaskIndex] = max_child_time + graph.times[TaskIndex][PP_ProcessorID];
-    return dp[TaskIndex];
+    return { TaskProcessors, TaskTimes };
 }
 
-std::tuple<std::vector<double>, double> BFS(TaskGraph& graph, int PP_ProcessorID) {
-    std::vector<int> dp(graph.tasks, -1);
-    CalculateDP(graph, 0, PP_ProcessorID, dp);
+std::tuple<std::vector<int>, std::vector<int>, int> BFS(const TaskGraph& graph, const std::vector<int>& TaskTimes, const std::vector<int>& TaskProcessors) {
+    std::vector in_degree(graph.tasks, 0);
 
-    std::vector<int> in_degree(graph.tasks, 0);
     for (int i = 0; i < graph.tasks; ++i) {
         for (const auto& edge : graph.graph[i]) {
             in_degree[edge.target]++;
         }
     }
 
-    auto compare = [&dp](int left, int right) {
-        return dp[left] < dp[right];
-    };
+    std::queue<int> queue;
+    for (int i = 0; i < graph.tasks; ++i) {
+        if (in_degree[i] == 0) {
+            queue.push(i);
+        }
+    }
 
-    std::priority_queue<int, std::vector<int>, decltype(compare)> heap(compare);
-    heap.push(0);
+    std::vector task_start_time(graph.tasks, 0);
+    std::vector earliest_start(graph.tasks, 0);
+    std::vector task_finish_time(graph.tasks, 0);
+    int makespan = 0;
 
-    std::vector<double> task_start_time(graph.tasks, 0.0);
-    double makespan = 0.0;
+    while (!queue.empty()) {
+        int current = queue.front();
+        queue.pop();
 
-    while (!heap.empty()) {
-        int current = heap.top();
-        heap.pop();
-
-        // we assume that time won't be -1
-        task_start_time[current] = makespan;
-        makespan += graph.times[current][PP_ProcessorID];
+        task_start_time[current] = earliest_start[current];
+        task_finish_time[current] = earliest_start[current] + TaskTimes[current];
+        makespan = std::max(makespan, task_finish_time[current]);
 
         for (const auto& edge : graph.graph[current]) {
-            in_degree[edge.target]--;
 
+            // Czekamy na rodziców
+            earliest_start[edge.target] = std::max(earliest_start[edge.target], task_finish_time[current]);
+
+            in_degree[edge.target]--;
             if (in_degree[edge.target] == 0) {
-                heap.push(edge.target);
+                queue.push(edge.target);
             }
         }
     }
-    return {task_start_time, makespan};
+    return { task_start_time, task_finish_time, makespan };
 }
 
-int CalculateCost(const TaskGraph& graph, int PP_ProcessorID) {
+int CalculateCost(const TaskGraph& graph, const std::vector<int>& TaskProcessors) {
     int total_cost = 0;
-
-    total_cost += graph.processors[PP_ProcessorID].cost;
+    std::vector<int> pe_usage(graph.proc, 0);
 
     for (int i = 0; i < graph.tasks; ++i) {
-        total_cost += graph.costs[i][PP_ProcessorID];
+        int p = TaskProcessors[i];
+        pe_usage[p]++;
+        total_cost += graph.costs[i][p];
+    }
+
+    for (int p = 0; p < graph.proc; ++p) {
+        if (pe_usage[p] > 0 && graph.processors[p].type == 1) {
+            total_cost += graph.processors[p].cost;
+        }
     }
 
     for (int c = 0; c < graph.bus; ++c) {
-        if (graph.channels[c].connected_processor[PP_ProcessorID] == 1) {
-            total_cost += graph.channels[c].cost;
-            break;
+        for (int p = 0; p < graph.proc; ++p) {
+            if (pe_usage[p] > 0 && graph.channels[c].connected_processor[p] == 1) {
+                if (graph.processors[p].type == 1) {
+                    total_cost += graph.channels[c].cost;
+                }
+                else {
+                    total_cost += graph.channels[c].cost * pe_usage[p];
+                }
+            }
         }
     }
 
     return total_cost;
 }
 
-int Choose_PP_Index(const TaskGraph& graph) {
-    for (int i = 0; i < graph.proc; ++i) {
-        if (graph.processors[i].type == 1) {
-            bool cant_do = false;
-            for (int j = 0; j < graph.tasks; ++j) {
-                if (graph.times[j][i] == -1) {
-                    cant_do = true;
-                    break;
-                }
-            }
-            if (!cant_do)
-                return i;
-        }
-    }
-    return -1;
-}
-
 int main() {
-    TaskGraph graph = readGraph("../input.txt");
+    std::string input;
+    std::cout << "Podaj nazwe pliku wejsciowego (razem z .txt): " << std::endl;
+    std::cin >> input;
+    TaskGraph graph = readGraph(input);
 
-    const int Used_PP_Processor = Choose_PP_Index(graph);
-    if (Used_PP_Processor == -1) {
-        throw std::runtime_error("Nie ma dostepnych procesorow typu PP, ktore moga wykonac wszystkie zadania");
-    }
-
-    auto [task_start_time, makespan] = BFS(graph, Used_PP_Processor);
+    auto [TaskProcessors, TaskTimes] = LowestTime(graph);
+    auto [task_start_time, task_finish_time, makespan] = BFS(graph, TaskTimes, TaskProcessors);
 
     std::cout << "Najszybszy czas calkowity (Makespan): " << makespan << "\n\n";
-    std::cout << "Koszt Architektury: " << CalculateCost(graph, Used_PP_Processor) << "\n\n";
+    std::cout << "Koszt wykonania zadania: " << CalculateCost(graph, TaskProcessors) << "\n\n";
     std::cout << "Wykorzystana Architektura:\n";
 
-    std::string prefix = "PP_" + std::to_string(Used_PP_Processor);
-    std::cout << prefix << ": ";
-    for (int i = 0; i < graph.tasks; ++i) {
-        std::cout << "T" << i << "(" << task_start_time[i] << ")";
-        if (i != graph.tasks - 1) std::cout << ", ";
+    int hc_counter = 0;
+    int pp_counter = 0;
+
+    for (int p = 0; p < graph.proc; ++p) {
+
+        std::string prefix;
+        if (graph.processors[p].type == 0) {
+            prefix = "HC" + std::to_string(hc_counter++);
+        }
+        else {
+            prefix = "PP" + std::to_string(pp_counter++);
+        }
+
+        std::cout << prefix << ": ";
+
+        bool has_tasks = false;
+        bool first_task = true;
+
+        for (int i = 0; i < graph.tasks; ++i) {
+            if (TaskProcessors[i] == p) {
+                has_tasks = true;
+
+                // Przecinek przed każdym kolejnym
+                if (!first_task) std::cout << ", ";
+
+                std::cout << "T" << i << "(" << task_start_time[i] << ")";
+                first_task = false;
+            }
+        }
+
+        if (!has_tasks) {
+            std::cout << "brak";
+        }
+
+        std::cout << "\n";
     }
 
-    std::cout << "\n";
+
+    return 0;
 }
---
+++ Task5/README.md
@@ -0,0 +1,12 @@
+##  Jak Uruchomić
+
+### 1. Przygotowanie pliku wejściowego z grafem
+
+### 2. Uruchomienie
+```
+    ./<nazwa_programu>
+```
+### 3. Program zapyta o nazwę pliku wejściowego (razem z rozszerzeniem .txt)
+
+### 4. Program zapyta o granice czasowa
+
